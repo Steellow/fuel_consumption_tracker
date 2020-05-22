f97ee8f9e16d36c70be9358810bc8058a61f764d
Steellow <hankilapio@gmail.com>
2020-05-22 12:39:51 +0300

    initial commit

---
+++ lib/models/log.dart
@@ -0,0 +1,11 @@
+class Log {
+  DateTime date;
+  double amount;
+  int odometer;
+
+  Log(DateTime date, double amount, int odometer) {
+    this.date = date;
+    this.amount = amount;
+    this.odometer = odometer;
+  }
+}
